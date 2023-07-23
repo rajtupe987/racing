8e0980e58828b9e10cffbd979658e8e7c173c234
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-07-24 01:28:39 +0530

    final-done

--- config/db.js
+++ config/db.js
@@ -1,18 +1,8 @@
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
-const connectToMongoDB = async () => {
-  try {
-    const connection = await mongoose.connect(process.env.mongo_url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-    return connection;
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    throw error; // Rethrow the error to be caught elsewhere if needed
-  }
-};
+const connection = mongoose.connect("mongodb+srv://Raj:Raj@cluster0.egewdp3.mongodb.net/ruralsnails?retryWrites=true&w=majority");
 
-module.exports = { connectToMongoDB };
+module.exports={connection}
+
