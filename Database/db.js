92b0e47f8331e12afaa836ca755bed1622b33928
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-07-23 23:28:37 +0530

    done

--- Database/db.js
+++ Database/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = () => {
-  return mongoose.connect(process.env.mongoDbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+  return mongoose.connect(process.env.mongoDbURL);
 };
 
 module.exports = { connectDB };
