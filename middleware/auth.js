a6714ff8b9691646941babd98b48018e1727df40
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-07-23 23:58:45 +0530

    done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,62 @@
-const jwt =require("jsonwebtoken");
+// const jwt =require("jsonwebtoken");
+// const {blacklistModel}=require("../model/blacklistmodel")
+
+// require("dotenv").config()
+// const authenticate=async(req,res,next)=>{
+//     const token=req.headers.authorization
+//     //console.log("token",token)
+//     try {
+//         if(token){
+//             const tokenblacklist=await blacklistModel.findOne({accessToken:token})
+//             //console.log("token",tokenblacklist)
+//             if(tokenblacklist){
+//                 return res.status(401).send({"msg":"please login"})
+//             }
+//             const decoded=jwt.verify(token,process.env.secrete_key)
+//             if(decoded){
+//                 req.body.email=decoded.email
+//                // console.log(req.body.email)
+//                 next()
+//             }else{
+//                 return res.status(400).send({"msg":"please login"})
+//             }
+
+//         }else{
+//             return res.status(400).send({"msg":"please login"})
+//         }
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send({"msg":"something weent wrong on authenticate"})
+//     }
+// }
+
+// module.exports={authenticate}
+
+
+const jwt =require('jsonwebtoken')
+const {userModel} = require("../models/user.model") ;
 const {blacklistModel}=require("../model/blacklistmodel")
-
 require("dotenv").config()
-const authenticate=async(req,res,next)=>{
-    const token=req.headers.authorization
-    //console.log("token",token)
+const authMiddleWare = async(req,res,next)=>{
     try {
-        if(token){
-            const tokenblacklist=await blacklistModel.findOne({accessToken:token})
-            //console.log("token",tokenblacklist)
-            if(tokenblacklist){
-                return res.status(401).send({"msg":"please login"})
-            }
-            const decoded=jwt.verify(token,process.env.secrete_key)
-            if(decoded){
-                req.body.email=decoded.email
-               // console.log(req.body.email)
-                next()
-            }else{
-                return res.status(400).send({"msg":"please login"})
-            }
-
-        }else{
-            return res.status(400).send({"msg":"please login"})
+        const token = req.headers.authorization;
+        const decodedToken = jwt.verify(token,process.env.secret);
+        const tokenblacklist=await blacklistModel.findOne({accessToken:token});
+        if(tokenblacklist){
+            return res.status(401).send({"msg":"please login"})
+         }
+        //after succesfully toekn varified we are decoding the toekn to extract userId
+        const {userId} = decodedToken;
+        //Checking if user exists
+        const user = await userModel.findById(userId);
+        if(!user){
+            return res.status(401).json({message:"User not found",ok:false})
         }
+        req.body.user = user._id;
+        next()
     } catch (error) {
-        console.log(error)
-        res.status(500).send({"msg":"something weent wrong on authenticate"})
+        return res.status(401).json({message:error.message})
     }
 }
 
-module.exports={authenticate}
+module.exports = {authMiddleWare}
