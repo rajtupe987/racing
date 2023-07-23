c0e8aa7e6dc2c5a918e3d31224664c5049b1c59d
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-07-23 23:49:11 +0530

    done

--- Controller/user.rout.js
+++ Controller/user.rout.js
@@ -12,89 +12,146 @@ const {blacklistModel}=require("../model/blacklistmodel")
 require("dotenv").config();
 // ************ register section************************
 
-router.post("/signup",async(req,res)=>{
-    try {
-        const {name,email,password,conformpassword}=req.body
-        if(!name){
-            return res.status(400).send({"ok": false,"msg":"name is required"})
-        }
-        if(!email){
-            return res.status(400).send({"ok": false,"msg":"email is required"})
-        }
-        if(!password){
-            return res.status(400).send({"ok": false,"msg":"password is required"})
-        }
-        if(!conformpassword){
-            return res.status(400).send({"ok": false,"msg":"conformpassword is required"})
-        }
+// router.post("/signup",async(req,res)=>{
+//     try {
+//         const {name,email,password,conformpassword}=req.body
+//         if(!name){
+//             return res.status(400).send({"ok": false,"msg":"name is required"})
+//         }
+//         if(!email){
+//             return res.status(400).send({"ok": false,"msg":"email is required"})
+//         }
+//         if(!password){
+//             return res.status(400).send({"ok": false,"msg":"password is required"})
+//         }
+//         if(!conformpassword){
+//             return res.status(400).send({"ok": false,"msg":"conformpassword is required"})
+//         }
 
        
-        const userExist= await userModel.findOne({email})
-        if(userExist){
-            return res.status(400).send({"ok": false,"msg":"email is already exist please signup"})
-        }
+//         const userExist= await userModel.findOne({email})
+//         if(userExist){
+//             return res.status(400).send({"ok": false,"msg":"email is already exist please signup"})
+//         }
        
-        bcrypt.hash(password,7,async(error,hash)=>{
-            if(error){
-                console.log("bcrypt",error)
-                return res.status(500).send({"ok": false,"msg":"something went wrong"})  
-            }
-            const user= new userModel({name,email,password:hash})
-             await user.save()
-             res.status(200).send({"ok": true,"msg":"register seccessfully"})
-        })  
-    } catch (error) {
-        //console.log(error)
-       res.status(500).send({"ok": false,"msg":"something went wrong "})
-    }
+//         bcrypt.hash(password,7,async(error,hash)=>{
+//             if(error){
+//                 console.log("bcrypt",error)
+//                 return res.status(500).send({"ok": false,"msg":"something went wrong"})  
+//             }
+//             const user= new userModel({name,email,password:hash})
+//              await user.save()
+//              res.status(200).send({"ok": true,"msg":"register seccessfully"})
+//         })  
+//     } catch (error) {
+//         //console.log(error)
+//        res.status(500).send({"ok": false,"msg":"something went wrong "})
+//     }
 
 
-})
+// })
 
-// ********************* login *************************
-router.post("/login",async(req,res)=>{
-    const {email,password}=req.body
+// // ********************* login *************************
+// router.post("/login",async(req,res)=>{
+//     const {email,password}=req.body
     
-    try {
-        if(!email){
-            return res.status(400).send({"ok": false,"msg":"put email"})
-        }
-        if(!password){
-            return res.status(400).send({"ok": false,"msg":"put password"})
-        }
-        const user=await userModel.findOne({email})
-        //console.log(user)
-        if(user){
-            bcrypt.compare(password,user.password,(error,result)=>{
-               if(result){
-                const token=jwt.sign({email},process.env.secrete_key,{expiresIn:"6h"})
-                const refreshtoken=jwt.sign({email},process.env.ref_key,{expiresIn:"24h"})
-                res.cookie("token",token,{maxAge:7*24*60*60*1000})
-                res.cookie("refreshToken",refreshtoken,{maxAge:7*24*60*60*1000})
+//     try {
+//         if(!email){
+//             return res.status(400).send({"ok": false,"msg":"put email"})
+//         }
+//         if(!password){
+//             return res.status(400).send({"ok": false,"msg":"put password"})
+//         }
+//         const user=await userModel.findOne({email})
+//         //console.log(user)
+//         if(user){
+//             bcrypt.compare(password,user.password,(error,result)=>{
+//                if(result){
+//                 const token=jwt.sign({email},process.env.secrete_key,{expiresIn:"6h"})
+//                 const refreshtoken=jwt.sign({email},process.env.ref_key,{expiresIn:"24h"})
+//                 res.cookie("token",token,{maxAge:7*24*60*60*1000})
+//                 res.cookie("refreshToken",refreshtoken,{maxAge:7*24*60*60*1000})
 
 
-                const response = {
-                    "approved": true,
-                    "token": token,
-                    "msg": "Login Successfull",
-                    "id": user._id,
-                    "userName": user.name
-                  }
-                  res.status(200).json(response)
-               }else{
-                return res.status(400).send({"ok": false,"msg":"wrong password"})
-               } 
-            })
-        }else{
-            return res.status(400).send({"ok": false,"msg":"put correct email id"})
-        }
-    } catch (error) {
-        res.status(400).send({"ok": false,"msg":"something went wrong"})
-    }
-})
+//                 const response = {
+//                     "approved": true,
+//                     "token": token,
+//                     "msg": "Login Successfull",
+//                     "id": user._id,
+//                     "userName": user.name
+//                   }
+//                   res.status(200).json(response)
+//                }else{
+//                 return res.status(400).send({"ok": false,"msg":"wrong password"})
+//                } 
+//             })
+//         }else{
+//             return res.status(400).send({"ok": false,"msg":"put correct email id"})
+//         }
+//     } catch (error) {
+//         res.status(400).send({"ok": false,"msg":"something went wrong"})
+//     }
+// })
 
 
 // ************ refreshtoken ************
+
+
+router.post("/signup", async (req, res) => {
+    const { name, email, password,conformpassword } = req.body;
+    const check = await UserModel.find({ email });
+    if (check.length > 0) {
+      return res.status(200).json({ "ok": false, "msg": "User already exist" });
+    }
+    bcrypt.hash(pass, 5, async (err, hash) => {
+      try {
+        if(err){
+          res.send(err.message)
+        }else{
+          const data = new userModel({ name, email, pass: hash });
+          await data.save();
+          res.status(200).json({ "ok": true, "msg": "Registered Successfully" });
+        }
+      } catch (error) {
+        res.status(400).json({ "ok": false, "msg": error.message });
+      }
+  
+    });
+  })
+  
+  router.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await userModel.findOne({ email:email });
+      if (!user) {
+        return res.status(401).json({ msg: "User with this email not found", ok: false })
+      }
+      const isPasswordSame = await bcrypt.compare(pass, user.pass)
+      if (!isPasswordSame) {
+        return res.status(401).json({ msg: "Invalid email or password", ok: false })
+      }
+  
+      //{ userId: user._id } == this is going to encoded into jwt
+      const token = jwt.sign({ userId: user._id }, process.env.secret, { expiresIn: '1hr' })
+      const refreshToken = jwt.sign({ userId: user._id }, process.env.refresh_secret, { expiresIn: "3hr" })
+      const response = {
+        "ok": true,
+        "token": token,
+        "msg": "Login Successfull",
+        "id": user._id,
+        "userName": user.name
+      }
+      res.status(200).json(response)
+    } catch (error) {
+      res.status(400).json({ "ok": false, "msg": error.message });
+    }
+  })
+
+
+
+
+
+
 router.get("/refreshtoken",async(req,res)=>{
     const refreshtoken = req.cookies.refreshToken;
     try {
